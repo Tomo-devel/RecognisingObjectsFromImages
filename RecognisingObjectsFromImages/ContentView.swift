6168c1137d639216fc49928b7ba7da6dc04081a1
Tomo-dev <124762208+tomo-devl@users.noreply.github.com>
2023-03-15 14:09:08 +0900

    Initial Commit

---
+++ RecognisingObjectsFromImages/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecognisingObjectsFromImages
+//
+//  Created by 井上智輝 on 2023-03-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecognisingObjectsFromImages/RecognisingObjectsFromImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognisingObjectsFromImagesApp.swift
+//  RecognisingObjectsFromImages
+//
+//  Created by 井上智輝 on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct RecognisingObjectsFromImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
